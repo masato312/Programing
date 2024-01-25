30d30db988e3e1cde9775b4477810892f5c88bff
masato312 <138412963+masato312@users.noreply.github.com>
2024-01-25 11:38:25 +0900

    2024/01/25

--- Game/DriveAndAvoid/Scene/ResultScene.h
+++ Game/DriveAndAvoid/Scene/ResultScene.h
@@ -1,1 +1,25 @@
 #pragma once
+
+#include "SceneBase.h"
+
+class ResultScene : public SceneBase
+{
+private:
+	int back_ground;			//”wŒi‰æ‘œ
+	int score;					//ƒXƒRƒA
+	int enemy_count[3];			//“G‚ÌƒJƒEƒ“ƒg
+	int enemy_image[3];			//“G‰æ‘œ
+
+public:
+	virtual void Initialize() override;
+	
+	virtual void Initialize() override;
+	virtual eScenetype Update() override;
+	virtual void Draw() const override;
+	virtual void Finalize() override;
+
+	virtual eScenetype GetNowScene()const override;
+
+private:
+	void ReadResultData();
+};
