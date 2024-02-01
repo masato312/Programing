8c1a86f3f7bda208daf041c99a5099f61e3f27cb
masato312 <138412963+masato312@users.noreply.github.com>
2024-02-01 17:17:10 +0900

    unti

--- Game/DriveAndAvoid/Scene/TitleScene.cpp
+++ Game/DriveAndAvoid/Scene/TitleScene.cpp
@@ -17,5 +17,94 @@ TitleScene::~TitleScene()
 void TitleScene::Initialize()
 {
 	//画像の読み込み
+	background_image = LoadGraph("Resource/images/Title.bmp");
+	menu_image = LoadGraph("Resource/images/menu.bmp");
+	cursor_image = LoadGraph("Resource/images/cone.bmp");
 
+	//エラーチェック
+	if (background_image == -1)
+	{
+		throw("Resource/images/Title.bmpがありません\n");
+	}
+	if (menu_image == -1)
+	{
+		throw("Resource/images/menu.bmpがありません\n");
+	}
+	if (cursor_image == -1)
+	{
+		throw("Resource/images/cone.bmpがありません\n");
+	}
+}
+
+//更新処理
+eScenetype TitleScene::Update()
+{
+	//カーソル↓移動
+	if (InputControl::GetButtonDown(XINPUT_BUTTON_DPAD_DOWN))
+	{
+		menu_cursor++;
+		//一番下に到達したら、一番上にする
+		if (menu_cursor > 3)
+		{
+			menu_cursor = 0;
+		}
+	}
+
+	//カーソル上移動
+	if (InputControl::GetButtonDown(XINPUT_BUTTON_DPAD_UP))
+	{
+		menu_cursor--;
+		//一番上に到達したら、一番下にする
+		if (menu_cursor < 0)
+		{
+			menu_cursor = 3;
+		}
+	}
+
+	//カーソル決定(決定した画面に遷移する)
+	if (InputControl::GetButtonDown(XINPUT_BUTTON_B))
+	{
+		switch (menu_cursor)
+		{
+		case 0:
+			return eScenetype::E_MAIN;
+		case 1:
+			return eScenetype::E_RANKING_DISP;
+		case 2:
+			return eScenetype::E_HELP;
+		case 3:
+			return eScenetype::E_END;
+
+		}
+	}
+	//現在のシーンタイプを返す
+	return GetNowScene();
+}
+
+//描画処理
+void TitleScene::Draw() const
+{
+	//タイトル画像の描画
+	DrawGraph(0, 0, background_image, FALSE);
+
+	//メニュー画像の描画
+	DrawGraph(120, 200, menu_image, TRUE);
+
+	//カーソル画像の描画
+	DrawRotaGraph(90, 220 + menu_cursor * 40, 0.7, DX_PI / 2.0, cursor_image, TRUE);
+}
+
+//終了時処理
+void TitleScene::Finalize()
+{
+	//読み込んだ画像の削除
+	DeleteGraph(background_image);
+	DeleteGraph(menu_image);
+	DeleteGraph(cursor_image);
+}
+
+//現在のシーン情報を取得
+eScenetype TitleScene::GetNowScene() const
+{
+	return eScenetype::E_TITLE;
 }
--- Game/DriveAndAvoid/Utility/Vector2D.h
+++ Game/DriveAndAvoid/Utility/Vector2D.h
@@ -1,1 +1,86 @@
 #pragma once
+
+//‚QŽŸŒ³ƒxƒNƒgƒ‹ƒNƒ‰ƒX
+class Vector2D
+{
+public:
+	float x;
+	float y;
+
+public:
+	Vector2D() :x(0.0f), y(0.0f)
+	{
+
+	}
+	Vector2D(float scalar) :x(scalar), y(scalar)
+	{
+
+	}
+	Vector2D(float mx, float my) :x(mx), y(my)
+	{
+
+	}
+
+public:
+	//‰‰ŽZŽq‚¨O‚ÎO‚ëO‚Ç
+	Vector2D& operator=(const Vector2D& location)
+	{
+		this->x = location.x;
+		this->y = location.y;
+
+		return *this;
+	}
+
+	const Vector2D operator + (const Vector2D& location)
+	{
+		float x = this->x + location.x;
+		float y = this->y + location.y;
+
+		return Vector2D(x,y);
+	}
+
+	const Vector2D operator-(const Vector2D& location)
+	{
+		float x = this->x - location.x;
+		float y = this->y - location.y;
+
+		return Vector2D(x,y);
+	}
+	const Vector2D operator*(const float& scalar)
+	{
+		float x = this->x * scalar;
+		float y = this->y * scalar;
+
+		return Vector2D(x,y);
+	}
+	const Vector2D operator*(const Vector2D& location)
+	{
+		float x = this->x * location.x;
+		float y = this->y * location.y;
+
+		return Vector2D(x, y);
+	}
+	const Vector2D operator/(const float& scalar)
+	{
+		if (scalar < 1e-6f)
+		{
+			return Vector2D(0.0f);
+		}
+		return Vector2D(this->x / scalar, this->y / scalar);
+	}
+	const Vector2D operator/(const Vector2D& location)
+	{
+		if (location.x < 1e-6f)
+		{
+			return Vector2D(0.0f); 
+		}
+		if (location.y < 1e-6f)
+		{
+			return Vector2D(0.0f);
+		}
+		return Vector2D(this->x / location.x, this->y / location.y);
+	}
+	Vector2D& operator+=(const Vector2D& location)
+
+
+};
